7c71059600b3769b87e7243679647cf640f92efe
MrPinkDog <rodrigofernandezserrano@gmail.com>
2024-03-08 13:49:03 +0100

    “Rfactor method divide() in branch developâ

--- src/main/java/org/iis/ajnebro/Calculator.java
+++ src/main/java/org/iis/ajnebro/Calculator.java
@@ -49,7 +49,8 @@ public class Calculator {
    * @throws IllegalArgumentException if number2 is zero.
    */
   public double divide(double number1, double number2) {
-    if (number2 == 0) {
+    boolean errorCondition = (number2 == 0);
+    if (errorCondition) {
       throw new IllegalArgumentException("Cannot divide by zero");
     }
     return number1 / number2;
